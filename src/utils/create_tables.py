4608cf53421104995be56be7dd3ce1bf13cb01e9
seun-beta <adegokeseunfunmi1999@gmail.com>
2022-05-28 13:42:13 +0100

    [UPD] Add test and delete files
    Signed-off-by: seun-beta <adegokeseunfunmi1999@gmail.com>

--- src/utils/create_tables.py
+++
@@ -1,6 +0,0 @@
-from services.models import *
-
-
-def create_tables():
-    with db:
-        db.create_tables([Transaction])
--- src/utils/recreate_tables.py
+++
@@ -1,9 +0,0 @@
-from services.models import db, Transaction
-
-
-MODELS = Transaction
-
-
-def recreate_tables():
-    db.drop_tables(MODELS)
-    db.create_tables(MODELS)
